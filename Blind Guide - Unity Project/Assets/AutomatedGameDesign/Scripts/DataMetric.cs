47adcb689b2634c9bf284a4d4242914df3e2b10a
Wasili <b.j.prattis@gmail.com>
2016-05-19 15:12:02 +0200

    Added local sqlite database

---
+++ Blind Guide - Unity Project/Assets/AutomatedGameDesign/Scripts/DataMetric.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DataMetric : MonoBehaviour
+{
+    [@System.Serializable]
+    public struct metricValue
+    {
+        public string name;
+        public float value;
+    };
+
+    public string metricName;
+    public string metricDetails;
+    public metricValue[] metricValues;
+    public string jsonData { get; private set; }
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
