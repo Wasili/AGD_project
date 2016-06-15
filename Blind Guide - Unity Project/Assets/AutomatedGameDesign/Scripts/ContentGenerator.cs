e50599671de268b103f3459fb1c4ec544fd0667b
Wasili <b.j.prattis@gmail.com>
2016-06-15 14:39:00 +0200

    Created base classes

---
+++ Blind Guide - Unity Project/Assets/AutomatedGameDesign/Scripts/ContentGenerator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ContentGenerator : MonoBehaviour {
+	public GameObject[] fireObjects, iceObjects, telekinesisObjects, destructionObjects;
+	public int levelDifficulty = 100;
+	public int maxDeathCountInfluence = 50;
+
+	private int playerDeathCount;
+	private DataMetricAttack.Type playerType;
+	private List<LevelSegment> segments;
+	
+	void Awake() {
+
+	}
+}
---
+++ Blind Guide - Unity Project/Assets/AutomatedGameDesign/Scripts/LevelSegment.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSegment : MonoBehaviour {
+	public enum Difficulty { easy, medium, hard }
+	public Difficulty difficulty;
+}
