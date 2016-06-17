ea08c95ec3c23aed69627141c0a8e7f13f52a732
Danny van der Mast <danny@vdmastnet.nl>
2016-06-17 11:30:10 +0200

    First version of localdb, has a few bugs

---
+++ Blind Guide - Unity Project/Assets/AutomatedGameDesign/Scripts/DataMetricLevel.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic; 
+
+public class DataMetricLevel : DataMetric {
+    
+    public enum Level { Tutorial, Fire1, Fire2, Fire3, Ice1, Ice2, Ice3, Jungle1, Jungle2, Jungle3 }
+
+    private int id;
+    public int levelID;
+    public float startTime;
+    public float endTime;
+    public int playerDied;
+    public string howPlayerDied;
+    private List<DataMetricObstacle> obstacles = new List<DataMetricObstacle>();
+    private List<DataMetricAttack> attacks = new List<DataMetricAttack>();
+
+    public void addAttack(DataMetricAttack attack)
+    {
+        Debug.Log("added attack...");
+        attacks.Add(attack);
+    }
+
+    public void addObstacle(DataMetricObstacle obstacle)
+    {
+        Debug.Log("Added obstacle...");
+        obstacles.Add(obstacle);
+    }
+
+    //public override void saveLocalData()
+    //{
+    //    queryForSave = "INSERT INTO level (gameID, level, startTime, endTime, playerDied, HowPlyerDied) VALUES("
+    //        + "'" + gameID + "'" + ","
+    //        + "'" + level.ToString() + "'" + ","
+    //        + "'" + startTime + "'" + ","
+    //        + "'" + endTime + "'" + ","
+    //        + "'" + playerDied + "'" + ","
+    //        + "'" + howPlayerDied + "'" + ")";  
+    //    DataCollector.getInstance().saveMetric(this);
+    //}
+
+    //public override string[] loadLocalData()
+    //{
+    //    queryforLoad = "";
+    //    return null;
+    //}
+}
---
+++ Blind Guide - Unity Project/Assets/AutomatedGameDesign/Scripts/GenGameInDB.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenGameInDB : MonoBehaviour {
+
+    private DataCollector inst;
+
+    public DataMetricLevel.Level level; 
+
+    void Awake()
+    {
+        inst = DataCollector.getInstance();
+
+        inst.createGame();
+        inst.startLevel(level); 
+    }
+}
